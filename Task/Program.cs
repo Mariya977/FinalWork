0af9a9ce6817af12c925a8a5a898f7d09ab9ceb2
Mariya977 <snezok971@yandex.ru>
2022-12-05 19:16:20 +0300

    task solved

---
+++ Task/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+string[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] newArray = CreateArray();
+ShowArray(newArray);
+Console.WriteLine();
+
+if (newArray.Length <= 3)
+{
+    Console.WriteLine(newArray);
+}
+else for (int i = 0; i < 3; i++)
+    {
+        Console.Write(newArray[i] + " ");
+    }
+    Console.WriteLine();
